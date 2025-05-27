e0ee6aa6e29b8850298b6b8063647b2e0c7b1ed5
Subhash Kumar <subhashit2025@gmail.com>
2025-05-28 02:02:45 +0530

    1st commit of 2nd class

--- 1.0Basics/function.js
+++ 1.0Basics/function.js
@@ -5,3 +5,34 @@ function add(a,b)
 }
 let result = add(10,20)
 console.log(result)
+
+let add1 = function(a,b)
+{
+    return a + b;
+}
+console.log(add1(20,40))
+
+let add2 =(a,b)=>{
+    return  a + b;
+
+}
+console.log(add2(40,50))
+
+let add3 = (a,b)=> a+b;
+console.log(add3(60,20));
+
+let add4 = (a) => a * 10;
+console.log(add4(70))
+
+let add5 = a=>a*20;
+console.log(add5(6))
+
+let add6 =()=>console.log('hello from function')
+add6()
+
+console.log(newadd(40,70));
+function newadd(a,b){
+    return a + b;
+}
+
+//let add=(10,20)=>console.log(a+b)
---
+++ 2.0Operators/index.js
@@ -0,0 +1,26 @@
+let x = 10;
+let y = 20;
+console.log( x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x ** y );
+
+// increment decrement operator
+
+let xx = 10;
+xx ++
+console.log(xx)
+xx--
+console.log(xx)
+xx += 5;
+console.log(xx)
+xx -= 4
+console.log(xx)
+
+console.log(1 === 1)
+console.log('1' === 1)
+console.log("-------------------------")
+console.log(1 == 1)
+console.log('1' == 1)
